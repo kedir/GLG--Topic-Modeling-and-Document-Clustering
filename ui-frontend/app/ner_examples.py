110fc421a2c8368acf98ab9b9956d3b8f11ccdf1
kedir <kedirhamid@gmail.com>
2022-11-23 21:01:59 +0000

    Add example.py files

--- ui-frontend/app/ner_examples.py
+++ ui-frontend/app/ner_examples.py
@@ -1,7 +1,7 @@
 NER_EXAMPLES = {
     
-    "Example 1":"Andrew Ng  is a professor at Stanford University.",
-    "Example 2":"AllenNLP is a PyTorch-based natural language processing library developed at the Allen Institute for Artificial Intelligence in Seattle.",
-    "Example 3":"Jeff Bezos founded e-commerce giant Amazon in 1994 out of his garage in Seattle.",
+    "Sentence 1":"Andrew Ng  is a professor at Stanford University.",
+    "Sentence 2":"AllenNLP is a PyTorch-based natural language processing library developed at the Allen Institute for Artificial Intelligence in Seattle.",
+    "Sentence 3":"Jeff Bezos founded e-commerce giant Amazon in 1994 out of his garage in Seattle.",
 
 }
